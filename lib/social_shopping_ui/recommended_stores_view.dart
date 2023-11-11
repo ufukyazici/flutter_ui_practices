68e65ca036a095697767591618affb310c55fc8d
ufukyazici <ufukyazici.dev@gmail.com>
2023-11-11 21:41:33 +0300

    Social Shopping UI

--- lib/social_shopping_ui/recommended_stores_view.dart
+++ lib/social_shopping_ui/recommended_stores_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kartal/kartal.dart';
 
 class RecommendedStoresView extends StatefulWidget {
   const RecommendedStoresView({super.key});
@@ -8,6 +9,7 @@ class RecommendedStoresView extends StatefulWidget {
 
 class _RecommendedStoresViewState extends State<RecommendedStoresView> {
   final String _title = 'Recommended Stores';
+  final String _followText = "FOLLOW";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,27 +21,81 @@ class _RecommendedStoresViewState extends State<RecommendedStoresView> {
         padding: const EdgeInsets.all(12),
         child: Column(
           children: [
-            SizedBox(
-                height: 300,
-                width: 400,
-                child: Card(
-                  child: Column(
-                    children: [
-                      ListTile(
-                        leading: ClipRRect(
-                            borderRadius: BorderRadius.circular(20),
-                            child:
-                                Image.network("https://xsgames.co/randomusers/assets/avatars/male/10.jpg", height: 45)),
-                        title: const Text('Ingredia Nutrisha'),
-                        subtitle: const Text('@Ingredia'),
-                        trailing: ElevatedButton(onPressed: () {}, child: const Text("FOLLOW")),
-                      )
-                    ],
-                  ),
-                )),
+            ListView.builder(
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return StoreCardWidget(followText: _followText);
+              },
+            )
           ],
         ),
       ),
     );
+  }
+}
+
+class StoreCardWidget extends StatelessWidget {
+  const StoreCardWidget({
+    super.key,
+    required String followText,
+  }) : _followText = followText;
+
+  final String _followText;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        height: 300,
+        width: 400,
+        child: Card(
+          child: Column(
+            children: [
+              ListTile(
+                leading: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.network("https://xsgames.co/randomusers/assets/avatars/male/10.jpg", height: 45)),
+                title: const Text('Ingredia Nutrisha'),
+                subtitle: const Text('@Ingredia'),
+                trailing: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(backgroundColor: const Color(0xfff8947c)),
+                    child: Text(_followText,
+                        style: Theme.of(context).textTheme.labelMedium?.copyWith(color: Colors.white))),
+              ),
+              const Divider(endIndent: 20, indent: 20),
+              Column(
+                children: [
+                  Container(
+                    height: 100,
+                    width: 350,
+                    color: context.general.randomColor,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 15, top: 8),
+                    child: Row(
+                      children: [
+                        Container(height: 80, width: 80, color: context.general.randomColor),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 9.0),
+                          child: Container(height: 80, width: 80, color: context.general.randomColor),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 9.0),
+                          child: Container(height: 80, width: 80, color: context.general.randomColor),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 9.0),
+                          child: Container(height: 80, width: 80, color: context.general.randomColor),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ));
   }
 }
