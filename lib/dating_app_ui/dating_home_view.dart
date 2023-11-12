9244aa57a6f8c0ddee28b5beaf354de61db14745
ufukyazici <ufukyazici.dev@gmail.com>
2023-11-12 18:47:10 +0300

    Dating App UI

--- lib/dating_app_ui/dating_home_view.dart
+++ lib/dating_app_ui/dating_home_view.dart
@@ -10,6 +10,7 @@ class _DatingHomeViewState extends State<DatingHomeView> {
   final String _title = "Matches";
   final String _titleDescription = 'This is a list of people who have like you and your matches.';
   final String _todayText = 'Today';
+  final List<DatingAppPersonModel> _persons = DummpyDatingApp().persons;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,10 +33,115 @@ class _DatingHomeViewState extends State<DatingHomeView> {
                   const Expanded(child: Divider(indent: 5, endIndent: 5))
                 ],
               ),
+              ListView.builder(
+                itemCount: 3,
+                itemBuilder: (context, index) {
+                  return DatingAppPersonCard(
+                      imageUrl: _persons[index].imageUrl,
+                      personName: _persons[index].personName,
+                      personAge: _persons[index].personAge);
+                },
+              )
             ],
           ),
         ),
       ),
     );
   }
+}
+
+class DatingAppPersonCard extends StatelessWidget {
+  const DatingAppPersonCard({
+    super.key,
+    required String imageUrl,
+    required String personName,
+    required String personAge,
+  })  : _imageUrl = imageUrl,
+        _personName = personName,
+        _personAge = personAge;
+
+  final String _imageUrl;
+  final String _personName;
+  final String _personAge;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        SizedBox(
+          height: 225,
+          width: 180,
+          child: Card(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.network(
+                _imageUrl,
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+        ),
+        Positioned.fill(
+            top: 170,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Column(children: [
+                Row(
+                  children: [
+                    Text(
+                      "$_personName $_personAge",
+                      style: Theme.of(context).textTheme.labelLarge?.copyWith(color: Colors.white),
+                      textAlign: TextAlign.start,
+                    ),
+                  ],
+                ),
+                IntrinsicHeight(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                          height: 20,
+                          width: 20,
+                          child: InkWell(
+                              onTap: () {},
+                              child: const Icon(
+                                Icons.close,
+                                color: Colors.white,
+                              ))),
+                      const VerticalDivider(color: Colors.white, thickness: 1),
+                      SizedBox(
+                          height: 20,
+                          width: 20,
+                          child: InkWell(
+                              onTap: () {},
+                              child: const Icon(
+                                Icons.favorite,
+                                color: Colors.white,
+                              ))),
+                    ],
+                  ),
+                )
+              ]),
+            ))
+      ],
+    );
+  }
+}
+
+class DatingAppPersonModel {
+  final String imageUrl;
+  final String personName;
+  final String personAge;
+
+  DatingAppPersonModel({required this.imageUrl, required this.personName, required this.personAge});
+}
+
+class DummpyDatingApp {
+  List<DatingAppPersonModel> persons = [
+    DatingAppPersonModel(imageUrl: "https://picsum.photos/200/300?random=1", personName: "Lilia", personAge: "19"),
+    DatingAppPersonModel(imageUrl: "https://picsum.photos/200/300?random=2", personName: "Lilia1", personAge: "22"),
+    DatingAppPersonModel(imageUrl: "https://picsum.photos/200/300?random=3", personName: "Lilia2", personAge: "34"),
+    DatingAppPersonModel(imageUrl: "https://picsum.photos/200/300?random=4", personName: "Lilia3", personAge: "20"),
+    DatingAppPersonModel(imageUrl: "https://picsum.photos/200/300?random=5", personName: "Lilia4", personAge: "40"),
+  ];
 }
