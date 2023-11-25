ccccc61be9c82a4b32b780c68d2b541cf77ba214
ufukyazici <ufukyazici.dev@gmail.com>
2023-11-25 17:38:38 +0300

    Gymnation UI

---
+++ lib/gymnation_ui/gymnation_email_verification_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class GymnationEmailVerificationView extends StatefulWidget {
+  const GymnationEmailVerificationView({super.key});
+  @override
+  State<GymnationEmailVerificationView> createState() => _GymnationEmailVerificationViewState();
+}
+
+class _GymnationEmailVerificationViewState extends State<GymnationEmailVerificationView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
