08f5fa2ba1a6a013a374e9c833a9e3cd3d25ccee
ufukyazici <ufukyazici.dev@gmail.com>
2023-11-26 17:22:17 +0300

    Trip & Travel UI

--- lib/trip_travel_ui/trip_travel_home_view.dart
+++ lib/trip_travel_ui/trip_travel_home_view.dart
@@ -8,6 +8,8 @@ class TripTravelHomeView extends StatefulWidget {
 
 class _TripTravelHomeViewState extends State<TripTravelHomeView> {
   final String randomImageUrl = "https://xsgames.co/randomusers/assets/avatars/female/1.jpg";
+  final String name = "Douglas Lyphe";
+  final String location = "New York, USA";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,13 +21,39 @@ class _TripTravelHomeViewState extends State<TripTravelHomeView> {
         ),
         centerTitle: true,
         actions: [
-          Padding(
-            padding: const EdgeInsets.only(left: 8.0),
-            child: CircleAvatar(
-              backgroundImage: NetworkImage(randomImageUrl),
-            ),
+          CircleAvatar(
+            backgroundImage: NetworkImage(randomImageUrl),
           )
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text(
+                  "Hi, $name",
+                  style: Theme.of(context).textTheme.titleMedium?.copyWith(fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                const Icon(
+                  Icons.location_on_outlined,
+                  size: 18,
+                  color: Colors.grey,
+                ),
+                Text(
+                  location,
+                  style:
+                      Theme.of(context).textTheme.titleSmall?.copyWith(fontWeight: FontWeight.bold, color: Colors.grey),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
